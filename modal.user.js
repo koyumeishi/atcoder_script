cb7a0168575d3c7958c06e8cde8461f094a57c46
koyumeishi.git <mis.to.re.17+github@gmail.com>
2015-11-05 05:40:49 +0900

    v0.02 standingsで上手く動作しないのを修正

--- modal.user.js
+++ modal.user.js
@@ -2,8 +2,8 @@
 // @name         AtCoderModalMoudarui
 // @namespace    koyumeishi_scripts_AtCoderModalMoudarui
 // @downloadURL  https://koyumeishi.github.io/atcoder_script/modal.user.js
-// @version      0.01
-// @description  disable modals
+// @version      0.02
+// @description  modal moudarui
 // @author       koyumeishi
 // @match        http://*.contest.atcoder.jp/*
 // @match        https://*.contest.atcoder.jp/*
@@ -11,8 +11,6 @@
 // ==/UserScript==
 
 (function(){
-  window.addEventListener("load", function(){
     document.getElementById('modal-contest-begin').setAttribute('id', 'modal-contest-begin-disabled');
     document.getElementById('modal-contest-end').setAttribute('id', 'modal-contest-end-disabled');
-  }, false);
 })();
